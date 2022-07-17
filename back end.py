a5490e2933c34c06eccda3d052b9aadd4aedac5f
FelisBieti <101007970+FelisBieti@users.noreply.github.com>
2022-07-17 20:45:14 +0800

    Update back end.py

--- back end.py
+++ back end.py
@@ -1,27 +1,28 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-from tensorflow.keras.models import load_model
+from keras.models import load_model
+from PIL import Image #use PIL
 import numpy as np
+
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def init():
     if request.method == 'POST':
         file = request.files['file']
         print("File Received")
         filename = secure_filename(file.filename)
         print(filename)
+        # Open the image form working directory
+        image = Image.open(file)
         model = load_model("Pneumonia")
-        from tensorflow.keras.preprocessing.image import img_to_array, load_img
-        img = load_img(file, False, target_size=(500, 500))
-        x = img_to_array(img) / 255.0
-        x = np.expand_dims(x, axis=0)
-        pred = model.predict(x)
-        return render_template("index1.html", result=str(pred))
+        img = np.asarray(image)
+        img.resize((150,150,3))
+        img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
+        img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
+        pred=model.predict(img)
+        return(render_template("index.html", result=str(pred)))
     else:
-        return render_template("index1.html", result="waiting")
-
-
+        return(render_template("index.html", result="WAITING"))
 if __name__ == "__main__":
     app.run()
