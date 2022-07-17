8be66552954b22ada4fdfb0c3449119959f4edcf
FelisBieti <101007970+FelisBieti@users.noreply.github.com>
2022-07-17 20:24:13 +0800

    Update back end.py

--- back end.py
+++ back end.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
 import numpy as np
 app = Flask(__name__)
@@ -8,15 +9,16 @@ app = Flask(__name__)
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
-        if file:
-            print("File Received")
-            model = load_model("Pneumonia")
-            from tensorflow.keras.preprocessing.image import img_to_array, load_img
-            img = load_img(file, False, target_size=(500, 500))
-            x = img_to_array(img) / 255.0
-            x = np.expand_dims(x, axis=0)
-            pred = model.predict(x)
-            return render_template("index1.html", result=str(pred))
+        print("File Received")
+        filename = secure_filename(file.filename)
+        print(filename)
+        model = load_model("Pneumonia")
+        from tensorflow.keras.preprocessing.image import img_to_array, load_img
+        img = load_img(file, False, target_size=(500, 500))
+        x = img_to_array(img) / 255.0
+        x = np.expand_dims(x, axis=0)
+        pred = model.predict(x)
+        return render_template("index1.html", result=str(pred))
     else:
         return render_template("index1.html", result="waiting")
 
